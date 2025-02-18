281d660965ab4ea213e4161e72ed19063f1c253e
ibrahimbadran2001 <ibraabaad@gmail.com>
2025-02-18 03:06:00 -0800

    #2 create splash view

---
+++ lib/core/utils/images_data.dart
@@ -0,0 +1,3 @@
+class ImagesData{
+  static String logo = 'assets/images/logo.png';
+}
---
+++ lib/spalsh/presentation/views/widgets/splash_body.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:vita/main.dart';
+import 'package:vita/spalsh/presentation/views/widgets/sliding_text.dart';
+
+import '../../../../core/utils/images_data.dart';
+
+
+class SplashBody extends StatefulWidget {
+  const SplashBody({super.key});
+
+  @override
+  State<SplashBody> createState() => _SplashBodyState();
+}
+
+class _SplashBodyState extends State<SplashBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+  @override
+  void initState() {
+    super.initState();
+    initSlidingAnimation();
+    navigateToHome();
+  }
+
+  void navigateToHome() {
+    Future.delayed(const Duration(seconds: 2), () {
+      Get.off(
+        () => const MyHomePage(title: 'test'),
+        transition: Transition.fade,
+        duration: const Duration(milliseconds: 750),
+      );
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    animationController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Image.asset(
+          height: 200,
+          width: 200,
+          ImagesData.logo,
+          fit: BoxFit.contain,
+        ),
+        const SizedBox(
+          height: 5,
+        ),
+        SlidingText(slidingAnimation: slidingAnimation),
+      ],
+    );
+  }
+
+  void initSlidingAnimation() {
+    animationController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 1));
+    slidingAnimation = Tween<Offset>(begin: Offset(0, 10), end: Offset.zero)
+        .animate(animationController);
+    animationController.forward();
+  }
+}
